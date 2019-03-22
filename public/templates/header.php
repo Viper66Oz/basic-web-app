99de2a2bd472305a5dd2166c2c3594bb30afe30e
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-22 14:39:06 -0400

    Update header.php

--- public/templates/header.php
+++ public/templates/header.php
@@ -13,8 +13,8 @@
                 <br>
                 <div class="container text-center">
                     <a class="btn btn-outline-success text-center" href="create.php" role="button">Add an Item</a>
-                    <a class="btn btn-outline-primary" href="#" role="button">Find an Item</a>
-                    <a class="btn btn-outline-warning" href="#" role="button">Update an Item</a>
-                    <a class="btn btn-outline-danger" href="#" role="button">Delete an Item</a>
+                    <a class="btn btn-outline-primary" href="read.php" role="button">Find an Item</a>
+                    <a class="btn btn-outline-warning" href="update.php" role="button">Update an Item</a>
+                    <a class="btn btn-outline-danger" href="delete.php" role="button">Delete an Item</a>
                 </div>
                 <br>
