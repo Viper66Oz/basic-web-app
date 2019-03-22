6b54c648b62cfd12c15184f7d192dca687de7466
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-22 10:26:31 -0400

    Create header.php

---
+++ public/templates/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Basic Web Application - Shane Jenkins u3118500</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="container">
+                <h1 class="text-center">House Inventory Application</h1>
+                <br>
+                <div class="container text-center">
+                    <a class="btn btn-outline-success text-center" href="#" role="button">Add an Item</a>
+                    <a class="btn btn-outline-primary" href="#" role="button">Find an Item</a>
+                    <a class="btn btn-outline-warning" href="#" role="button">Update an Item</a>
+                    <a class="btn btn-outline-danger" href="#" role="button">Delete an Item</a>
+                </div>
+                <br>
