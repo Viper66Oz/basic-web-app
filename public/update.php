caad529f9ea2d51b34230176a2ffec1a58de82ad
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-26 08:46:41 -0400

    Update update.php
    page styled

--- public/update.php
+++ public/update.php
@@ -14,7 +14,7 @@ try {
 <?php include"templates/header.php"; ?>
 <h2>Update and item</h2>
 <?php foreach($result as $row) { ?>
-<p>
+<!--<p>
     ID:
     <?php echo escape($row['id']); ?><br>
     Item:<?php echo $row['item']; ?><br>
@@ -30,7 +30,56 @@ try {
     Picture:<?php echo $row['picture']; ?><br>
     Description:<?php echo $row['description']; ?><br>
     <a class="btn btn-outline-warning" href="update-work.php?id=<?php echo $row['id']; ?>">Update</a>
-</p>
+</p>-->
+<div class="row">
+    <div class="col-sm-4 alert alert-info" role="alert">
+        ID: <?php echo escape($row['id']); ?>
+    </div>
+    <div class="col-sm-4 alert alert-success" role="alert">
+        Item: <?php echo $row['item']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success" role="alert">
+        Room: <?php echo $row['room']; ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4 alert alert-success">
+        Make/Brand: <?php echo $row['makebrand']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success">
+        Model: <?php echo $row['model']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success">
+        Serial Number: <?php echo $row['serialnumber']; ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4 alert alert-success">
+        Purchase Price: <?php echo $row['purchaseprice']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success">
+        Purchase Date: <?php echo $row['purchasedate']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success">
+        Place of Purchase: <?php echo $row['purchaseplace']; ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4 alert alert-success">
+        Receipt: <?php echo $row['receipt']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success">
+        Heirloom/Antique: <?php echo $row['heirloomantique']; ?>
+    </div>
+    <div class="col-sm-4 alert alert-success">
+        Picture: <img src="<?php echo $row['picture']; ?>">
+    </div>
+</div>
+<div class="row">
+    <div class="col alert alert-success">
+        Description: <?php echo $row['description']; ?><a class="btn btn-warning float-right" href="update-work.php?id=<?php echo $row['id']; ?>">Update item</a>
+    </div>
+</div>
 <hr>
 <?php };
 ?>
