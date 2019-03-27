31c98ceddfd0b6204570269e491d58a02633c4ff
Ben Ennis Butler <ben@beneb.com>
2019-03-27 21:08:48 +1100

    Fix to update work page - see email

--- public/templates/header.php
+++ public/templates/header.php
@@ -1,30 +1,32 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <title>Basic Web Application - Shane Jenkins u3118500</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div class="container">
-                <h1 class="text-center">House Inventory Application</h1>
-                <br>
-                <div class="container text-center">
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <a class="btn btn-outline-success text-center" href="create.php" role="button">Add an Item</a>
-                        </div>
-                        <div class="col-sm-3">
-                            <a class="btn btn-outline-primary" href="read.php" role="button">Find an Item</a>
-                        </div>
-                        <div class="col-sm-3">
-                            <a class="btn btn-outline-warning" href="update.php" role="button">Update an Item</a>
-                        </div>
-                        <div class="col-sm-3">
-                            <a class="btn btn-outline-danger" href="delete.php" role="button">Delete an Item</a>
-                        </div>
+
+<head>
+    <title>Basic Web Application - Shane Jenkins u3118500</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/stylesheet.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <h1 class="text-center">House Inventory Application</h1>
+            <br>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <a class="btn btn-outline-success text-center" href="create.php" role="button">Add an Item</a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a class="btn btn-outline-primary" href="read.php" role="button">Find an Item</a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a class="btn btn-outline-warning" href="update.php" role="button">Update an Item</a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a class="btn btn-outline-danger" href="delete.php" role="button">Delete an Item</a>
                     </div>
                 </div>
-                <br>
+            </div>
+            <br>
--- public/update-work.php
+++ public/update-work.php
@@ -33,8 +33,7 @@ if (isset($_POST['submit'])) {
         purchaseplace = :purchaseplace,
         receipt = :receipt,
         heirloomantique = :heirloomantique,
-        description = :description,
-        date = :date
+        description = :description
         WHERE id = :id";
         $statement = $connection->prepare($sql);
         $statement->execute($item);
@@ -62,41 +61,41 @@ if (isset($_GET['id'])) {
     echo "No id - something went wrong";
 };
 ?>
-<?php include "templates/header.php"; ?>
-<h2>Update selected item</h2>
-<form method="post" class="was-validated">
-    <div class="form-group">
-        <div class="form-row">
-            <!--<div class="col-sm">
-                <label for="id">ID:</label>
-                <input type="text" class="form-control" name="id" id="id" value="<?php echo escape($item['id']); ?>" >
-            </div>-->
-            <div class="col-sm">
-                <label for="item">Item:</label>
-                <input type="text" class="form-control" name="item" value="<?php echo escape($item['item']); ?>" required>
+    <?php include "templates/header.php"; ?>
+    <h2>Update selected item</h2>
+    <form method="post" class="was-validated">
+        <div class="form-group">
+            <div class="form-row">
+                <div class="col-sm id-hide">
+                    <label for="id">ID:</label>
+                    <input type="text" class="form-control" name="id" id="id" value="<?php echo escape($item['id']); ?>" readonly>
+                </div>
+                <div class="col-sm">
+                    <label for="item">Item:</label>
+                    <input type="text" class="form-control" name="item" value="<?php echo escape($item['item']); ?>" required>
+                </div>
+                <div class="col-sm">
+                    <label for="item">Room:</label>
+                    <input type="text" class="form-control" name="room" value="<?php echo escape($item['room']); ?>" required>
+                </div>
             </div>
-            <div class="col-sm">
-                <label for="item">Room:</label>
-                <input type="text" class="form-control" name="room" value="<?php echo escape($item['room']); ?>" required>
+            <br>
+            <div class="form-row">
+                <div class="col-sm">
+                    <label for="item">Make/Brand:</label>
+                    <input type="text" class="form-control" name="makebrand" value="<?php echo escape($item['makebrand']); ?>" required>
+                </div>
+                <div class="col-sm">
+                    <label for="item">Model:</label>
+                    <input type="text" class="form-control" name="model" value="<?php echo escape($item['model']); ?>" required>
+                </div>
+                <div class="col-sm">
+                    <label for="item">Serial Number:</label>
+                    <input type="text" class="form-control" name="serialnumber" value="<?php echo escape($item['serialnumber']); ?>" required>
+                </div>
             </div>
-        </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <label for="item">Make/Brand:</label>
-                <input type="text" class="form-control" name="makebrand" value="<?php echo escape($item['makebrand']); ?>" required>
-            </div>
-            <div class="col-sm">
-                <label for="item">Model:</label>
-                <input type="text" class="form-control" name="model" value="<?php echo escape($item['model']); ?>" required>
-            </div>
-            <div class="col-sm">
-                <label for="item">Serial Number:</label>
-                <input type="text" class="form-control" name="serialnumber" value="<?php echo escape($item['serialnumber']); ?>" required>
-            </div>
-        </div>
-        <br>
-        <div class="form-row">
+            <br>
+            <div class="form-row">
                 <div class="col-sm">
                     <label for="item">Purchase Price:</label>
                     <input type="number" class="form-control" name="purchaseprice" value="<?php echo escape($item['purchaseprice']); ?>" required>
@@ -110,58 +109,85 @@ if (isset($_GET['id'])) {
                     <input type="text" class="form-control" name="purchaseplace" value="<?php echo escape($item['purchaseplace']); ?>" required>
                 </div>
             </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <span>Receipt:</span>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="receipt" id="receipt-yes" value="<?php echo escape($item['receipt']); ?>" required>
-                    <label class="form-check-label" for="receipt-yes">Yes</label>
+            <br>
+            <div class="form-row">
+                <div class="col-sm">
+                    <span>Receipt:</span>
+                    <div class="form-check-inline">
+
+                        <!--                        BEN: New radio button logic to return correct value. 
+                            You can correct spacing in CSS -->
+
+                        <input class="form-check-input" type="radio" name="receipt" id="receipt-yes" value="yes" required <?php echo ($item[ 'receipt']=='yes' ) ? 'checked' : ''; ?>>
+                        <label class="form-check-label" for="receipt-yes">Yes</label>
+
+                        <input class="form-check-input" type="radio" name="receipt" id="receipt-No" value="no" required <?php echo ($item[ 'receipt']=='no' ) ? 'checked' : ''; ?>>
+                        <label class="form-check-label" for="receipt-yes">No</label>
+
+
+                        <!--
+                        <input class="form-check-input" type="radio" name="receipt" id="receipt-yes" value="<?php echo escape($item['receipt']); ?>" required>
+                        <label class="form-check-label" for="receipt-yes">Yes</label>
+-->
+                    </div>
+                    <!--
+                    <div class="form-check-inline">
+                        <input class="form-check-input" type="radio" name="receipt" id="receipt-no" value="<?php echo escape($item['receipt']); ?>" required>
+                        <label class="form-check-label" for="receipt-no">No</label>
+                    </div>
+-->
                 </div>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="receipt" id="receipt-no" value="<?php echo escape($item['receipt']); ?>" required>
-                    <label class="form-check-label" for="receipt-no">No</label>
-                </div>
-            </div>
-            <div class="col-sm">
-                <span>Family heirloom or antique:</span>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="heirloomantique" id="antique-yes" value="<?php echo escape($item['heirloomantique']); ?>" required>
-                    <label class="form-check-label" for="antique-yes">Yes</label>
-                </div>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="heirloomantique" id="antique-no" value="<?php echo escape($item['heirloomantique']); ?>" required>
-                    <label class="form-check-label" for="antique-no">No</label>
-                </div>
-            </div>
-            <div class="col-sm">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" name="picture" value="<?php echo escape($item['picture']); ?>">
-                    <label class="custom-file-label" for="picture">Upload a picture...</label>
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <label for="item">Description:</label>
-                <input type="text" class="form-control" name="description" value="<?php echo escape($item['description']); ?>">
-            </div>
-        </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <input type="submit" name="submit" value="Update item" class="btn btn-warning">
-            </div>
-        </div>
-    </div>
-</form>
-<hr>
-<?php if (isset($_POST['submit']) && $statement) : ?>
-<div class="row">
-    <div class="col-sm alert alert-success">
-        Item successfully updated.
-    </div>
+                <div class="col-sm">
+                    <span>Family heirloom or antique:</span>
+                    <div class="form-check-inline">
+                        <!--                        <input class="form-check-input" type="radio" name="heirloomantique" id="antique-yes" value="<?php //echo escape($item['heirloomantique']); ?>" required>-->
+
+
+                        <input class="form-check-input" type="radio" name="heirloomantique" id="antique-yes" value="yes" required <?php echo ($item[ 'heirloomantique']=='yes' ) ? 'checked' : ''; ?>>
+                        <label class="form-check-label" for="antique-yes">Yes</label>
+
+                        <input class="form-check-input" type="radio" name="heirloomantique" id="antique-no" value="no" required <?php echo ($item[ 'heirloomantique']=='no' ) ? 'checked' : ''; ?>>
+                        <label class="form-check-label" for="antique-no">No</label>
+
+
+
+
+                    </div>
+                    <!--
+<div class="form-check-inline">
+    <input class="form-check-input" type="radio" name="heirloomantique" id="antique-no" value="<?php echo escape($item['heirloomantique']); ?>" required>
+    <label class="form-check-label" for="antique-no">No</label>
 </div>
-<?php endif; ?>
-<?php include"templates/footer.php"; ?>
+-->
+                </div>
+                <div class="col-sm">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="picture" value="<?php echo escape($item['picture']); ?>">
+                        <label class="custom-file-label" for="picture">Upload a picture...</label>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="form-row">
+                <div class="col-sm">
+                    <label for="item">Description:</label>
+                    <input type="text" class="form-control" name="description" value="<?php echo escape($item['description']); ?>">
+                </div>
+            </div>
+            <br>
+            <div class="form-row">
+                <div class="col-sm">
+                    <input type="submit" name="submit" value="Update item" class="btn btn-warning">
+                </div>
+            </div>
+        </div>
+    </form>
+    <hr>
+    <?php if (isset($_POST['submit']) && $statement) : ?>
+    <div class="row">
+        <div class="col-sm alert alert-success">
+            Item successfully updated.
+        </div>
+    </div>
+    <?php endif; ?>
+    <?php include"templates/footer.php"; ?>
