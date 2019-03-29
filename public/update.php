dda01bbf9f58a88958c9ea948e6c0bd930634d86
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-28 22:36:16 -0400

    Update update.php

--- public/update.php
+++ public/update.php
@@ -12,61 +12,65 @@ try {
 }
 ?>
 <?php include"templates/header.php"; ?>
-<h2>Update an item</h2>
+<div class="container">
+    <h2>Update an item</h2>
+</div>
 <?php foreach($result as $row) { ?>
-<div class="row">
-    <!--<div class="col-sm alert alert-info" role="alert">
-        <strong>ID: </strong><?php echo escape($row['id']); ?>
-    </div>-->
-    <div class="col-sm alert alert-success" role="alert">
-        <strong>Item: </strong><?php echo $row['item']; ?>
+<div class="container">
+    <div class="row">
+        <!--<div class="col-sm alert alert-info" role="alert">
+            <strong>ID: </strong><?php echo escape($row['id']); ?>
+        </div>-->
+        <div class="col alert alert-success" role="alert">
+            <strong>Item: </strong><?php echo $row['item']; ?>
+        </div>
+        <div class="col alert alert-success" role="alert">
+            <strong>Room: </strong><?php echo $row['room']; ?>
+        </div>
     </div>
-    <div class="col-sm alert alert-success" role="alert">
-        <strong>Room: </strong><?php echo $row['room']; ?>
+    <div class="row">
+        <div class="col alert alert-success">
+            <strong>Make/Brand: </strong><?php echo $row['makebrand']; ?>
+        </div>
+        <div class="col alert alert-success">
+            <strong>Model: </strong><?php echo $row['model']; ?>
+        </div>
+        <div class="col alert alert-success">
+            <strong>Serial Number: </strong><?php echo $row['serialnumber']; ?>
+        </div>
     </div>
+    <div class="row">
+        <div class="col alert alert-success">
+            <strong>Purchase Price: </strong><?php echo $row['purchaseprice']; ?>
+        </div>
+        <div class="col alert alert-success">
+            <strong>Purchase Date: </strong><?php echo $row['purchasedate']; ?>
+        </div>
+        <div class="col alert alert-success">
+            <strong>Place of Purchase: </strong><?php echo $row['purchaseplace']; ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col alert alert-success">
+            <strong>Receipt: </strong><?php echo $row['receipt']; ?>
+        </div>
+        <div class="col alert alert-success">
+            <strong>Heirloom/Antique: </strong><?php echo $row['heirloomantique']; ?>
+        </div>
+        <div class="col alert alert-success">
+            <strong>Picture: </strong><img src="<?php echo $row['picture']; ?>">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-10 alert alert-success">
+            <strong>Description: </strong><?php echo $row['description']; ?>
+        </div>
+        <div class="col-2 alert">
+            <a class="btn btn-warning float-right" href="update-work.php?id=<?php echo $row['id']; ?>">Update item</a>
+        </div>
+    </div>
+    <hr>
 </div>
-<div class="row">
-    <div class="col-sm alert alert-success">
-        <strong>Make/Brand: </strong><?php echo $row['makebrand']; ?>
-    </div>
-    <div class="col-sm alert alert-success">
-        <strong>Model: </strong><?php echo $row['model']; ?>
-    </div>
-    <div class="col-sm alert alert-success">
-        <strong>Serial Number: </strong><?php echo $row['serialnumber']; ?>
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm alert alert-success">
-        <strong>Purchase Price: </strong><?php echo $row['purchaseprice']; ?>
-    </div>
-    <div class="col-sm alert alert-success">
-        <strong>Purchase Date: </strong><?php echo $row['purchasedate']; ?>
-    </div>
-    <div class="col-sm alert alert-success">
-        <strong>Place of Purchase: </strong><?php echo $row['purchaseplace']; ?>
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm alert alert-success">
-        <strong>Receipt: </strong><?php echo $row['receipt']; ?>
-    </div>
-    <div class="col-sm alert alert-success">
-        <strong>Heirloom/Antique: </strong><?php echo $row['heirloomantique']; ?>
-    </div>
-    <div class="col-sm alert alert-success">
-        <strong>Picture: </strong><img src="<?php echo $row['picture']; ?>">
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm-10 alert alert-success">
-        <strong>Description: </strong><?php echo $row['description']; ?>
-    </div>
-    <div class="col-sm-2 alert">
-        <a class="btn btn-warning float-right" href="update-work.php?id=<?php echo $row['id']; ?>">Update item</a>
-    </div>
-</div>
-<hr>
 <?php };
 ?>
 <?php include "templates/footer.php"; ?>
