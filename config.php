04439ae780367730b6dc4a1b2a52921daa33037e
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-28 22:35:22 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host       = "103.67.235.20";
-$username   = "skratchd_JenxOz";
-$password   = "RsWHPn7vHV4uSPh";
-$dbname     = "skratchd_inventory_app";
+$host       = "103.67.235.20";/*localhost*/
+$username   = "skratchd_JenxOz";/*root*/
+$password   = "RsWHPn7vHV4uSPh";/*root*/
+$dbname     = "skratchd_inventory_app";/*inventory*/
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
