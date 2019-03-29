bf685a8e9f1b0eec39ea31313c2eaaa05760b8bb
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-28 22:35:31 -0400

    Update create.php

--- public/create.php
+++ public/create.php
@@ -65,111 +65,101 @@ if (isset($_POST['submit'])) {
 }
 ?>
 <?php include "templates/header.php"; ?>
-<form method="post" class="was-validated">
-    <div class="form-group">
-        <!--This is for the username/password area of the form-->
-        <!--<div class="form-row">
-            <div class="col">
-                <label for="username">Username:</label>
-                <input type="text" class="form-control" name="username" id="username" required minlength="5" maxlength="15">
-                <small id="usernameHelp" class="form-text text-muted">Password must be 5-15 characters long and only contain letters. No spaces or special characters.
-                </small>
-            </div>
-            <div class="col">
-                <label for="password">Password:</label>
-                <input type="password" class="form-control" name="password" id="password" required minlength="8" maxlength="15">
-                <small id="passwordHelp" class="form-text text-muted">Password must be 8-15 characters long and only contain letters. No spaces or special characters.
-                </small>
-            </div>
-        </div>-->
-        <br>
- 
-        <div class="form-row">
-            <div class="col-sm">
-                <label for="item">Item:</label>
-                <input type="text" class="form-control" name="item" id="item" required>
-            </div>
-            <div class="col-sm">
-                <label for="room">Room:</label>
-                <input type="text" class="form-control" name="room" id="room" required>
-            </div>
-        </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <label for="makebrand">Make/Brand:</label>
-                <input type="text" class="form-control" name="makebrand" id="makebrand" required>
-            </div>
-            <div class="col-sm">
-                <label for="model">Model:</label>
-                <input type="text" class="form-control" name="model" id="model" required>
-            </div>
-            <div class="col-sm">
-                <label for="serialnumber">Serial Number:</label>
-                <input type="text" class="form-control" name="serialnumber" id="serialnumber" required>
-            </div>
-        </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <label for="purchaseprice">Purchase Price:</label>
-                <input type="number" class="form-control" name="purchaseprice" id="purchaseprice" required>
-            </div>
-            <div class="col-sm">
-                <label for="purchasedate">Purchase Date:</label>
-                <input type="date" class="form-control" name="purchasedate" id="purchasedate" required>
-            </div>
-            <div class="col-sm">
-                <label for="purchaseplace">Place of Purchase:</label>
-                <input type="text" class="form-control" name="purchaseplace" id="purchaseplace" required>
-            </div>
-        </div>
-        <br>
-        <div class="form-row">
-            <div class="col-sm">
-                <span>Receipt:</span>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="receipt" id="receipt-yes" value="yes">
-                    <label class="form-check-label" for="receipt-yes">Yes</label>
+<div class="container">
+    <h2>Add Item</h2>
+</div>
+<div class="container">
+    <form method="post" class="was-validated">
+        <div class="form-group">
+            <div class="form-row">
+                <div class="col">
+                    <label for="item">Item:</label>
+                    <input type="text" class="form-control" name="item" id="item" required>
                 </div>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="receipt" id="receipt-no" value="no" checked>
-                    <label class="form-check-label" for="receipt-no">No</label>
+                <div class="col">
+                    <label for="room">Room:</label>
+                    <input type="text" class="form-control" name="room" id="room" required>
                 </div>
             </div>
-            <div class="col-sm">
-                <span>Family heirloom or antique:</span>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="heirloomantique" id="antique-yes" value="yes">
-                    <label class="form-check-label" for="antique-yes">Yes</label>
+            <br>
+            <div class="form-row">
+                <div class="col">
+                    <label for="makebrand">Make/Brand:</label>
+                    <input type="text" class="form-control" name="makebrand" id="makebrand" required>
                 </div>
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="radio" name="heirloomantique" id="antique-no" value="no" checked>
-                    <label class="form-check-label" for="antique-no">No</label>
+                <div class="col">
+                    <label for="model">Model:</label>
+                    <input type="text" class="form-control" name="model" id="model" required>
+                </div>
+                <div class="col">
+                    <label for="serialnumber">Serial Number:</label>
+                    <input type="text" class="form-control" name="serialnumber" id="serialnumber" required>
                 </div>
             </div>
-            <div class="col-sm">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="picture" name="picture">
-                    <label class="custom-file-label" for="picture">Upload a picture...</label>
+            <br>
+            <div class="form-row">
+                <div class="col">
+                    <label for="purchaseprice">Purchase Price:</label>
+                    <input type="number" class="form-control" name="purchaseprice" id="purchaseprice" required>
+                </div>
+                <div class="col">
+                    <label for="purchasedate">Purchase Date:</label>
+                    <input type="date" class="form-control" name="purchasedate" id="purchasedate" required>
+                </div>
+                <div class="col">
+                    <label for="purchaseplace">Place of Purchase:</label>
+                    <input type="text" class="form-control" name="purchaseplace" id="purchaseplace" required>
+                </div>
+            </div>
+            <br>
+            <div class="form-row">
+                <div class="col">
+                    <span>Receipt:</span>
+                    <div class="form-check-inline">
+                        <input class="form-check-input" type="radio" name="receipt" id="receipt-yes" value="yes">
+                        <label class="form-check-label" for="receipt-yes">Yes</label>
+                    </div>
+                    <div class="form-check-inline">
+                        <input class="form-check-input" type="radio" name="receipt" id="receipt-no" value="no" checked>
+                        <label class="form-check-label" for="receipt-no">No</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <span>Family heirloom or antique:</span>
+                    <div class="form-check-inline">
+                        <input class="form-check-input" type="radio" name="heirloomantique" id="antique-yes" value="yes">
+                        <label class="form-check-label" for="antique-yes">Yes</label>
+                    </div>
+                    <div class="form-check-inline">
+                        <input class="form-check-input" type="radio" name="heirloomantique" id="antique-no" value="no" checked>
+                        <label class="form-check-label" for="antique-no">No</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="picture" name="picture">
+                        <label class="custom-file-label" for="picture">Upload a picture...</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <label for="description">Description</label>
+                    <textarea class="form-control is-invalid" id="description" name="description" placeholder="Add a description to the text area"></textarea>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col alert">
+                    <input type="submit" value="Add Item" id="submit" name="submit" class="btn btn-outline-success">
+                    <input type="reset" value="Clear Item" id="clear" class="btn btn-outline-warning">
                 </div>
             </div>
         </div>
-        <div class="form-row">
-            <div class="col-sm">
-                <label for="description">Description</label>
-                <textarea class="form-control is-invalid" id="description" name="description" placeholder="Add a description to the text area"></textarea>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="col-sm alert">
-                <input type="submit" value="Add Item" id="submit" name="submit" class="btn btn-outline-success">
-                <input type="reset" value="Clear Item" id="clear" class="btn btn-outline-warning">
-            </div>
-        </div>
-    </div>
-</form>
+    </form>
+</div>
 <?php if (isset($_POST['submit']) && $statement) { ?>
-<div class="alert alert-success" role="alert">Item successfully added.</div>
+<div class="container">
+    <div class="alert alert-success" role="alert">Item successfully added.</div>
+</div>
 <?php } ?> 
 <?php include "templates/footer.php"; ?>
