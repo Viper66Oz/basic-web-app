e3511c709c980e1a6eb4461b47d9d635632b3a9b
Viper66Oz <shane.jenkins72@gmail.com>
2019-03-28 22:36:19 -0400

    Create welcome.php

---
+++ public/welcome.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<?php include "templates/header.php"; ?>
+<?php include "template/footer.php"; ?>
